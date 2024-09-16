8b313269f3905af3a0097e74eeb5023353f18bbf
Manish Sharma <manish.sharma120298@gmail.com>
2024-09-17 00:13:51 +0530

    ecommerce-website-with-react

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Navbar.css";
+
+import rocket from "../../assets/rocket.png";
+
+const Navbar = () => {
+	return (
+		<nav className="align_center navbar">
+			<div className="align_center">
+				<h1 className="navbar_heading">CartWish</h1>
+				<form className="align_center navbar_form">
+					<input type="text" className="navbar_search" placeholder="Search Products"></input>
+					<button type="submit" className="search_button">
+						Search
+					</button>
+				</form>
+			</div>
+			<div className="navbar_links">
+				<a href="#" className="align_center">
+					Home <img src={rocket} alt="" className="link_emoji" />
+				</a>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
